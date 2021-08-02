6e3a9179cdf2f366bde48668eb1e5973e692c92e
Ruturaj <sant.ruturaj2001@gmail.com>
2021-08-02 18:35:04 +0530

    added create room feature and added to database

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CreateRoomBtnModal from './CreateRoomBtnModal';
 import DashboardToggle from './dashboard/DashboardToggle';
 
 const Sidebar = () => {
@@ -6,8 +7,8 @@ const Sidebar = () => {
     <div className="h-100 pt-2">
       <div>
         <DashboardToggle />
+        <CreateRoomBtnModal />
       </div>
-      Bottom
     </div>
   );
 };
