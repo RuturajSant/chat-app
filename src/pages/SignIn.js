b8f961cc42066f3b4c5bb0241ce9f0f5030f53ae
Ruturaj <sant.ruturaj2001@gmail.com>
2021-08-20 12:27:20 +0530

    made app PWA and changed name

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -38,7 +38,7 @@ const SignIn = () => {
                     <Col xs={24} md={12} mdOffset={6}>
                         <Panel>
                             <div className="text-center">
-                            <h2>Welcome to Chatta</h2>
+                            <h2>Welcome to R-Chat</h2>
                             <p>Progressive Web Chat</p>
                             </div>
                             <div className="mt-3">
